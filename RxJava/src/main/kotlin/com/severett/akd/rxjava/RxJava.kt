b5ee6b9a76c60a75f87268861adaf4244b31316b
Severn Everett <severn.everett@gmail.com>
2023-03-05 16:12:07 +0100

    Updating RxJava demo

--- RxJava/src/main/kotlin/com/severett/akd/rxjava/RxJava.kt
+++ RxJava/src/main/kotlin/com/severett/akd/rxjava/RxJava.kt
@@ -4,28 +4,37 @@ import io.reactivex.rxjava3.core.Observable
 import java.util.stream.*
 
 fun main() {
-    // runStream()
-    runRxJava()
+    try {
+        runStream()
+    } catch (e: Exception) {
+        e.printStackTrace(System.err)
+    }
+    try {
+        runRxJava()
+    } catch (e: Exception) {
+        e.printStackTrace(System.err)
+    }
 }
 
 private fun runStream() {
-    Stream.iterate(0) { i -> i + 1 }
+    generateSequence(0) { i -> i + 1 }
         .filter { i -> i % 5 != 0 }
-        .map { i -> i to "NUMBER $i" }
-        .limit(100)
+        .map { i -> if (i == 118) throw Exception("Something bad!") else i to "NUMBER $i" }
+        .take(100)
         .forEach { pair ->
-            if (pair.first == 118) throw Exception("Something bad!")
-            println("ENTRY: ${pair.second} (i ${pair.first})")
+            println("STREAM ENTRY: ${pair.second} (i ${pair.first})")
         }
 }
 
 private fun runRxJava() {
     Observable.fromStream(Stream.iterate(0) { i -> i + 1 })
         .filter { i -> i % 5 != 0 }
+        // .map { i -> if (i == 118) throw Exception("Something bad!") else i to "NUMBER $i" }
         .map { i -> i to "NUMBER $i" }
+        .onErrorComplete { println(it.message); true }
         .take(100)
         .forEach { pair ->
             if (pair.first == 118) throw Exception("Something bad!")
-            println("ENTRY: ${pair.second} (i ${pair.first})")
+            println("RXJAVA ENTRY: ${pair.second} (i ${pair.first})")
         }
 }
